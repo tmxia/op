eb4a6be785318bffcbd76226070fe29bc74504d7
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-17 22:30:16 +0800

    Apply automatic changes

---
+++ README.md
@@ -0,0 +1,29 @@
+# 简介
+
+本固件适配斐讯 N1 旁路由模式，追求轻量（请注意：不具备 PPPoE、WiFi 相关功能）。<br>
+固件包含默认皮肤、完整 IPv6 支持，以及下列 luci-app：<br>
+
+- `luci-app-amlogic`
+- `luci-app-argon-config`
+- `luci-app-ddns-go`
+- `luci-app-dockerman`
+- `luci-app-passwall`
+- `luci-app-https-dns-proxy`
+- `luci-app-ksmbd`
+- `luci-app-nft-qos`
+- `luci-app-ttyd`
+- `luci-app-uhttpd`
+- `luci-ssl-openssl`
+- `luci-theme-argon`
+
+---
+
+# 致谢
+
+- [immortalwrt/immortalwrt](https://github.com/immortalwrt/immortalwrt/tree/openwrt-24.10)
+- [unifreq/openwrt_packit](https://github.com/unifreq/openwrt_packit)
+- [ophub/kernel](https://github.com/ophub/kernel/releases/tag/kernel_flippy)
+- [breakingbadboy/OpenWrt](https://github.com/breakingbadboy/OpenWrt/releases/tag/kernel_stable)
+- [nantayo/N1-OpenWrt](https://github.com/nantayo/N1-OpenWrt)
+- [god-org/immortalwrt](https://github.com/god-org/immortalwrt)
+- [恩山论坛帖子](https://www.right.com.cn/forum/thread-4076037-1-1.html)
---
+++ n1/diy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
+
+# Git sparse clone
+git_sparse_clone() {
+    branch="$1" repourl="$2" && shift 2
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+    cd "$repodir" && git sparse-checkout set "$@"
+    mv -f "$@" ../package
+    cd .. && rm -rf "$repodir"
+}
+
+# Modify default theme
+sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
+
+# Add packages
+git clone --single-branch --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+
+# Update packages
+rm -rf feeds/luci/applications/luci-app-passwall
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
