2627190160e378118538f185bc47b6ac9b9511e4
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-20 00:49:15 +0800

    Add files via upload

---
+++ n1-lede/diy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-design/luci-theme-Bootstrap/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-argon/luci-theme-Bootstrap/g' feeds/luci/collections/luci/Makefile
+
+# Changing the host name
+sed -i 's/ImmortalWrt/n1/g' package/base-files/files/bin/config_generate
+
+# Git sparse clone
+git_sparse_clone() {
+    branch="$1" repourl="$2" && shift 2
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+    cd "$repodir" && git sparse-checkout set "$@"
+    mv -f "$@" ../package
+    cd .. && rm -rf "$repodir"
+}
+
+# Add packages
+git clone --single-branch --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+
+# Update packages
+rm -rf feeds/luci/applications/luci-app-passwall
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
+
+# Pip3 conf
+mkdir -p ~/.pip
+echo "[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+trusted-host = pypi.tuna.tsinghua.edu.cn" > ~/.pip/pip.conf
+
+# Pip3 packages
+pip3 install requests telethon tqdm paramiko tailer flask-cors unrar pytz bleach beautifulsoup4 python-dateutil docker
+
+
