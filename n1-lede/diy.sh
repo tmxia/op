4aae2a535f998552a0de180f662f268e83031253
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-22 11:07:56 +0800

    Update diy.sh

--- n1-lede/diy.sh
+++ n1-lede/diy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #
 
+# Git sparse clone
+git_sparse_clone() {
+    branch="$1" repourl="$2" && shift 2
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+    cd "$repodir" && git sparse-checkout set "$@"
+    mv -f "$@" ../package
+    cd .. && rm -rf "$repodir"
+}
+
 # Modify default IP   第一行19.07的路径   第二行23.05的路径
 #sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/luci2/bin/config_generate
@@ -15,7 +25,6 @@ sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view
 
 # 拉取passwall
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview}
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
