4e57d2a9f96a1737487ad13317182f7bed9e97ae
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-25 01:50:30 +0800

    Update diy.sh

--- n1-lede/diy.sh
+++ n1-lede/diy.sh
@@ -29,16 +29,16 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
 
 git clone --depth=1 https://github.com/kenzok8/small-package package/small
-mv package/small/luci-app-openclash package/luci-app-openclash
-mv package/small/iptvhelper package/iptvhelper
-mv package/small/luci-app-iptvhelper package/luci-app-iptvhelper
-mv package/small/luci-app-timecontrol package/luci-app-timecontrol
-mv package/small/cdnspeedtest package/cdnspeedtest
-mv package/small/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
-mv package/small/luci-app-dnsfilter package/luci-app-dnsfilter
+#mv package/small/luci-app-openclash package/luci-app-openclash
+#mv package/small/iptvhelper package/iptvhelper
+#mv package/small/luci-app-iptvhelper package/luci-app-iptvhelper
+#mv package/small/luci-app-timecontrol package/luci-app-timecontrol
+#mv package/small/cdnspeedtest package/cdnspeedtest
+#mv package/small/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
+#mv package/small/luci-app-dnsfilter package/luci-app-dnsfilter
 mv package/small/luci-app-fileassistant package/luci-app-fileassistant
-rm -rf feeds/luci/applications/luci-app-wol
-mv package/small/luci-app-wolplus feeds/luci/applications/luci-app-wolplus
+#rm -rf feeds/luci/applications/luci-app-wol
+#mv package/small/luci-app-wolplus feeds/luci/applications/luci-app-wolplus
 # mv package/small/luci-app-msd_lite package/luci-app-msd_lite
 # mv package/small/netdata package/netdata
 # mv package/small/luci-app-netdata package/luci-app-netdata
@@ -47,7 +47,7 @@ mv package/small/luci-app-wolplus feeds/luci/applications/luci-app-wolplus
 # mv package/small/qt6tools package/qt6tools
 # mv package/small/rblibtorrent package/rblibtorrent
 # mv package/small/luci-app-qbittorrent package/luci-app-qbittorrent
-mv package/small/luci-app-wechatpush package/luci-app-wechatpush
+#mv package/small/luci-app-wechatpush package/luci-app-wechatpush
 mv package/small/luci-app-poweroff package/luci-app-poweroff
 # mv package/small/wrtbwmon package/wrtbwmon
 # mv package/small/luci-app-wrtbwmon package/luci-app-wrtbwmon
@@ -61,10 +61,8 @@ git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 
-
-
 git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # Remove packages
@@ -93,8 +91,8 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/luci/applications/luci-app-serverchan
 
 # 1 启用 frps
-rm -rf feeds/packages/net/frp
-git clone https://github.com/kuoruan/openwrt-frp feeds/packages/net/frp
+#rm -rf feeds/packages/net/frp
+# git clone https://github.com/kuoruan/openwrt-frp feeds/packages/net/frp
 # git clone https://github.com/kuoruan/openwrt-frp -b v0.53.2-1 feeds/packages/net/frp
 # git clone https://github.com/user1121114685/frp.git feeds/packages/net/frp
 # rm -rf feeds/luci/applications/luci-app-frps
@@ -114,7 +112,7 @@ sed -i 's/LEDE/N1/g' package/base-files/luci2/bin/config_generate
 
 # Modify default IP   第一行19.07的路径   第二行23.05的路径
 #sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.6.50/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/luci2/bin/config_generate
 
 # golang版本修复
 rm -rf feeds/packages/lang/golang
