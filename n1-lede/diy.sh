ad8df2d3f277ba8135e25807106cdfdf006c6ed2
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-26 14:45:39 +0800

    Update diy.sh

--- n1-lede/diy.sh
+++ n1-lede/diy.sh
@@ -14,7 +14,7 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 # #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
@@ -26,6 +26,16 @@ git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-
 # git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
 # git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
 # Remove packages
+
+# Pip3 conf
+mkdir -p ~/.pip
+echo "[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+trusted-host = pypi.tuna.tsinghua.edu.cn" > ~/.pip/pip.conf
+
+# Pip3 packages
+pip3 install requests telethon tqdm paramiko tailer flask-cors unrar pytz bleach beautifulsoup4 python-dateutil docker
+
 #删除lean库中的插件，使用自定义源中的包。
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
