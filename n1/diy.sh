693cda385de8f05724b21b8bd85c5c4de5ba45ce
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-28 02:33:41 +0800

    Update diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-design/luci-theme-Bootstrap/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-design/luci-theme-Bootstrap/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-argon/luci-theme-Bootstrap/g' feeds/luci/collections/luci/Makefile
 
 # Changing the host name
