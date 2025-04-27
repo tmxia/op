2aa6e996aa3f1cde89dc036f543d3609b11f2b63
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-27 18:51:22 +0800

    Update diy.sh

--- n1-lede/diy.sh
+++ n1-lede/diy.sh
@@ -41,3 +41,6 @@ sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generat
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+
+# 修改默认主题为bootstrap
+sed -i 's|option theme.*|option theme "bootstrap"|' /etc/config/luci
