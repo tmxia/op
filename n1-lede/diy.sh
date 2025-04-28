6a4fe5f426a3c385ee1934868b7640091ae477b7
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-28 09:12:24 +0800

    Update diy.sh

--- n1-lede/diy.sh
+++ n1-lede/diy.sh
@@ -36,6 +36,10 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/luci/themes/luci-theme-design
 #rm -rf feeds/luci/applications/luci-app-design-config
 
+# golang版本修复
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
 # 恢复默认主题
 sed -i 's/luci-theme-argon/luci-theme-bootstrap/' feeds/luci/collections/luci/Makefile
 
@@ -44,5 +48,3 @@ sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generat
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
-
-
