2c238a3a8f4e13cb2331d14224129851b2b63ca5
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-30 02:48:30 +0800

    Update diy-part2.sh

--- n1-le/diy-part2.sh
+++ n1-le/diy-part2.sh
@@ -4,7 +4,7 @@
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.3/g' package/base-files/files/bin/config_generate
 
 # => dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
@@ -18,10 +18,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 
 # => ssr plus
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 # => OpenClash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
 
 # => mosdns
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
