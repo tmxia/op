417bd2febfd2e7a9aadc732c122f45560ed87a22
tmxia <tmxia1000@gmail.com>
2025-10-22 14:43:51 +0800

    更新 diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -32,6 +32,60 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 # cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
+# 创建nikki规则文件包目录
+mkdir -p package/nikki-files/files/etc/nikki/run
+
+# 创建Makefile
+cat > package/nikki-files/Makefile << 'EOF'
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=nikki-files
+PKG_VERSION:=1.0
+PKG_RELEASE:=1
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/nikki-files
+  SECTION:=utils
+  CATEGORY:=Utilities
+  TITLE:=Nikki rule files
+endef
+
+define Package/nikki-files/description
+  Pre-downloaded rule files for Nikki (geosite.dat and geoip.metadb)
+endef
+
+define Build/Prepare
+endef
+
+define Build/Configure
+endef
+
+define Build/Compile
+endef
+
+define Package/nikki-files/install
+	$(INSTALL_DIR) $(1)/etc/nikki/run
+	$(INSTALL_DATA) ./files/etc/nikki/run/geosite.dat $(1)/etc/nikki/run/
+	$(INSTALL_DATA) ./files/etc/nikki/run/geoip.metadb $(1)/etc/nikki/run/
+endef
+
+$(eval $(call BuildPackage,nikki-files))
+EOF
+
+# 下载规则文件到包目录
+echo "下载规则文件中..."
+wget -O package/nikki-files/files/etc/nikki/run/geosite.dat https://cdn.uuiu.net/nikki/geosite.dat
+wget -O package/nikki-files/files/etc/nikki/run/geoip.metadb https://cdn.uuiu.net/nikki/geoip.metadb
+
+# 设置文件权限
+chmod 755 package/nikki-files/files/etc/nikki/run/geosite.dat
+chmod 755 package/nikki-files/files/etc/nikki/run/geoip.metadb
+
+# 更新feeds并安装nikki-files包
+./scripts/feeds update nikki-files
+./scripts/feeds install -a -p nikki-files
+
 # Pip3 conf
 mkdir -p ~/.pip
 echo "[global]
@@ -43,4 +97,3 @@ pip3 install requests telethon tqdm paramiko tailer flask-cors unrar pytz bleach
 
 # Clean packages
 rm -rf clone
-
